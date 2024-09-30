d141a46931d95989e732078a8fb2846f2ae5dd11
Neel Bullywon <85001440+Nebu0528@users.noreply.github.com>
2024-09-29 23:40:29 -0400

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -4,10 +4,13 @@ from data_loader import load_python_code_data
 class CodeToDocInference:
     #Initialization of T5 Model
     def __init__(self, model_path="models/t5_code_to_text/"):
+        #load the model and tokenizer
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
 
     def generate_doc(self, code_snippet):
+        #define the inputs and outputs
         inputs = self.tokenizer(code_snippet, return_tensors="pt", padding=True, truncation=True)
         outputs = self.model.generate(inputs['input_ids'], max_new_tokens=200)
+        #decode and return the results
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
