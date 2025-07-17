24a7434d732f88d9f3665df81253baaccd77f15a
Neel Bullywon <n2bullyw@uwaterloo.ca>
2025-07-17 17:39:25 -0400

    Required to ensure the model can actually retain information

--- src/model.py
+++ src/model.py
@@ -31,9 +31,8 @@ class code_doc:
                 optimizer.zero_grad()
                 outputs = self.model(input_ids=inputs.input_ids, labels=labels)
                 loss = outputs.loss
-                #look into this, possibly not needed
-                #loss.backward()
-                #optimizer.step()
+                loss.backward()
+                optimizer.step()
 
                 #display training process (testing purposes)
                 #print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}")
